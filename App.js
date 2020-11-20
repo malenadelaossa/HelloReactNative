37d3e3ac27b9925960030de077213542ea775b50
Malena de la Ossa <malenadelaossa@hotmail.com>
2020-11-19 20:48:23 -0600

    Layout Using Flex

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+
 
 
 
@@ -16,13 +16,30 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 const App: () => React$Node = () => {
   return(
 <View>
-  <Text></Text>
+  <Text style={styles.welcome}>Hola Mundo</Text>
+  <Text style={[styles.welcome, {flex:1}]}>Hola Mundo</Text>
+  <Text style={styles.welcome}>Hola Mundo</Text>
+  <Text style={styles.welcome}>Hola Mundo</Text>
 </View>
   );
 }
   
 const styles = StyleSheet.create({
-  
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    //alignItems: 'center',
+    //margin: 10,
+    backgroundColor:'orange',
+    justifyContent: 'space-around',
+    },
+  welcome:{
+    backgroundColor: 'pink',    
+    fontSize:20,
+    textAlign: 'center',
+    margin: 10,
+    flex: 2,
+  },
 });
 
 export default App;
