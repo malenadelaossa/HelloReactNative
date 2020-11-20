b439b67c537eb2fc7fc530d950c15d4d1a06ddac
Malena de la Ossa <malenadelaossa@hotmail.com>
2020-11-19 20:06:21 -0600

    Nuevo

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+
+
+
+const App: () => React$Node = () => {
+  return(
+<View>
+  <Text></Text>
+</View>
+  );
+}
+  
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
